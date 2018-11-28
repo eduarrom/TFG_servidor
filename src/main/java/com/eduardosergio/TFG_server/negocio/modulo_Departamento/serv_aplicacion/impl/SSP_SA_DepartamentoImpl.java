a5e730a407d1623aef585baf4849db2dbae7fae0
Eduardo Romero Palencia <eduarrom@ucm.es>
2018-11-28 21:54:19 +0100

    SSP Correccion error

--- src/main/java/com/eduardosergio/TFG_server/negocio/modulo_Departamento/serv_aplicacion/impl/SSP_SA_DepartamentoImpl.java
+++ src/main/java/com/eduardosergio/TFG_server/negocio/modulo_Departamento/serv_aplicacion/impl/SSP_SA_DepartamentoImpl.java
@@ -1,19 +1,24 @@
 package com.eduardosergio.TFG_server.negocio.modulo_Departamento.serv_aplicacion.impl;
 
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.eduardosergio.TFG_server.negocio.modulo_Departamento.Authenticator;
 import com.eduardosergio.TFG_server.negocio.modulo_Departamento.serv_aplicacion.SSP_SA_Departamento;
-import com.rodrigo.TFG_server.Negocio.Modulo_Departamento.Entidad.Transfers.TDepartamentoCompleto
-import com.rodrigo.TFG_cliente.Negocio.Modulo_Departamento.Entidad.Transfers.TDepartamentoCompleto;
-import com.rodrigo.TFG_cliente.Negocio.Modulo_Departamento.Excepciones.DepartamentoException;
-import com.rodrigo.TFG_cliente.Negocio.Modulo_Empleado.Delegado.impl.Delegado_EmpleadoImpl;
+import com.rodrigo.TFG_server.Negocio.Modulo_Departamento.Entidad.Transfers.TDepartamentoCompleto;
+import com.rodrigo.TFG_server.Negocio.Modulo_Departamento.Excepciones.DepartamentoException;
+import com.rodrigo.TFG_server.Negocio.Modulo_Departamento.Excepciones.DepartamentoFieldInvalidException;
 
 public class SSP_SA_DepartamentoImpl implements SSP_SA_Departamento {
 	
-	private static Logger log = LoggerFactory.getLogger(Delegado_EmpleadoImpl.class);
-
-    private SSP_SA_Departamento portDepartamento;
+	private Logger log = LoggerFactory.getLogger(SSP_SA_DepartamentoImpl.class);
 
     private String HOST = "https://localhost" ;
 
@@ -21,17 +26,58 @@ public class SSP_SA_DepartamentoImpl implements SSP_SA_Departamento {
 
     private String APP_URI = "/TFG_server/services";
 
+    private final String SERVICE_NAME = "/SA_Departamento/departamento";
 
-    private final String URL_WSDL = "http://localhost:8080/TFG_server/wsdl/SA_Departamento.wsdl";
+    private final String URL_SERVICIO = HOST + ":" + PORT + APP_URI + SERVICE_NAME;
 
-    private final String URL_SERVICE = HOST + ":"+ PORT + APP_URI + "/SA_DepartamentoSOAP";
 
-    private final String NAMESPACE_URI = "http://impl.Serv_aplicacion.modulo_departamento.negocio.TFG_server.eduardosergio.com/";
 
-    private final String SERVICE_NAME = "Broker_SA_DepartamentoImpl";
-	
-	@Override
-	public TDepartamentoCompleto buscarByID(Long id) throws DepartamentoException {
-		return portDepartamento.buscarByID(id);
-	}
+    private Client cliente;
+
+
+    public SSP_SA_DepartamentoImpl() {
+        log.info("Creando DelegadoDelNegocio");
+
+        log.info("Creando cliente");
+        cliente = ClientBuilder
+                .newBuilder()
+                .newClient()
+                .register(new Authenticator("user", "pass"));
+
+        log.info("DelegadoDelNegocio creado");
+    }
+    
+    @Override
+    public TDepartamentoCompleto buscarByID(Long id) throws DepartamentoException {
+        log.info("Delegado_DepartamentoImpl.buscarDepartamentoByID");
+        log.info("id = [" + id + "]");
+
+        String urlFinal = URL_SERVICIO + "";
+
+        System.out.println("urlFinal = [" + urlFinal + "/" + id.toString() + "]");
+
+
+        try {
+            WebTarget wt = cliente.target(urlFinal);
+            wt = wt.path(id.toString());
+            Invocation.Builder b = wt.request();
+            TDepartamentoCompleto dept = b.get(TDepartamentoCompleto.class);
+
+            System.out.println("dept = [" + dept + "]");
+            return dept;
+
+        } catch (BadRequestException e) {
+
+            System.out.println("Bad request");
+            throw new DepartamentoFieldInvalidException("ERROR EN LA PETICION!");
+
+        } catch (NotFoundException e) {
+
+            System.out.println("Not found");
+            return null;
+        }
+        
+    }
+
+
 }
