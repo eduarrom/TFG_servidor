732d244d8c85cd4f79b96d46403521bd276a4161
Eduardo Romero Palencia <eduarrom@ucm.es>
2019-01-30 10:36:18 +0100

    Message Inspector hecho

--- src/main/java/com/eduardosergio/TFG_server/seguridad/messageInterceptorGateway/MessageInterceptorGateway.java
+++ src/main/java/com/eduardosergio/TFG_server/seguridad/messageInterceptorGateway/MessageInterceptorGateway.java
@@ -1,6 +1,8 @@
 package com.eduardosergio.TFG_server.seguridad.messageInterceptorGateway;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -8,6 +10,8 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class MessageInterceptorGateway implements Filter{
 
@@ -22,8 +26,22 @@ FilterConfig filterConfig= null;
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		System.out.println("ESTOY EN EL MIG");
-		chain.doFilter(request, response);
+		
+		//String user = ((HttpServletRequest) request).getUserPrincipal().getName();
+		//Como asi podia sacar el usuario pero la contraseña no he optado por sacar las credenciales
+		//del header
+		
+		String b64credentials = ((HttpServletRequest) request).getHeader("Authorization").substring("Basic".length()).trim();
+	    String credentials = new String(Base64.getDecoder().decode(b64credentials), StandardCharsets.UTF_8);
+	    
+		if (credentials.equals("usuario:contra")) {
+			chain.doFilter(request, response);
+		} else {
+			HttpServletResponse resp = (HttpServletResponse) response;
+			
+			resp.sendError(400);
+		}
+		
 		
 	}
 
