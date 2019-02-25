abe145428b67b5f50159bb21b7fafe5295b65aed
Eduardo Romero Palencia <eduarrom@ucm.es>
2019-02-25 14:39:05 +0100

    Message Inspector leyendo ip y credenciales

--- src/main/java/com/eduardosergio/TFG_server/seguridad/messageInterceptorGateway/MessageInterceptorGateway.java
+++ src/main/java/com/eduardosergio/TFG_server/seguridad/messageInterceptorGateway/MessageInterceptorGateway.java
@@ -4,6 +4,10 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.cxf.helpers.IOUtils;
 import org.apache.cxf.interceptor.Fault;
@@ -23,6 +27,28 @@ public class MessageInterceptorGateway extends AbstractPhaseInterceptor<Message>
 		
         try {
         	
+        	HttpServletRequest req = (HttpServletRequest)message.get("HTTP.REQUEST");
+        	HttpServletResponse res = (HttpServletResponse)message.get("HTTP.RESPONSE");
+        	
+        	String ipAddress = req.getHeader("X-FORWARDED-FOR");  
+            if (ipAddress == null) {  
+              ipAddress = req.getRemoteAddr();
+            }
+            
+            if (ipAddress.equals("192.168.2.1")) {
+            	res.sendError(500);
+            }
+
+            String b64credentials = req.getHeader("Authorization").substring("Basic".length()).trim();
+    	    String credentials = new String(Base64.getDecoder().decode(b64credentials), StandardCharsets.UTF_8);
+    	    
+    	    System.out.println(credentials);
+    	    if (!credentials.equals("usuario:contra") && !credentials.equals("user:pass")) {
+
+    			res.sendError(500);
+    		}
+            
+    	    /*
         	InputStream reader = message.getContent(InputStream.class);
     		
         	CachedOutputStream writer = new CachedOutputStream();
@@ -32,6 +58,7 @@ public class MessageInterceptorGateway extends AbstractPhaseInterceptor<Message>
         	System.out.println(content);
         	
         	message.setContent(InputStream.class, new ByteArrayInputStream(content.getBytes(StandardCharsets.UTF_8)));
+			*/
 
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
