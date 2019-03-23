9bd9d22f89d020893c922ea899a7914fde303bac
Eduardo Romero Palencia <eduarrom@ucm.es>
2019-03-23 14:37:37 +0100

    STS Funcionando en HelloWorld

--- src/main/java/com/eduardosergio/TFG_server/negocio/helloWorld/HelloWorld.java
+++ src/main/java/com/eduardosergio/TFG_server/negocio/helloWorld/HelloWorld.java
@@ -3,6 +3,7 @@ package com.eduardosergio.TFG_server.negocio.helloWorld;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
+import javax.xml.ws.soap.Addressing;
 
 import org.apache.cxf.annotations.Policies;
 import org.apache.cxf.annotations.Policy;
@@ -11,15 +12,15 @@ import org.apache.cxf.annotations.Policy;
 public interface HelloWorld {
 	
 	@Policies({
+		@Policy(uri="policy.xml",
+				placement=Policy.Placement.BINDING_OPERATION,
+				includeInWSDL=true),
 		@Policy(uri="inputPolicy.xml",
 				placement=Policy.Placement.BINDING_OPERATION_INPUT,
 				includeInWSDL=true),
 		@Policy(uri="outputPolicy.xml",
 				placement=Policy.Placement.BINDING_OPERATION_OUTPUT,
-				includeInWSDL=true),
-		@Policy(uri="policy.xml",
-				placement=Policy.Placement.BINDING_OPERATION,
-				includeInWSDL=true),
+				includeInWSDL=true)
 	})
 	
 	@WebMethod
